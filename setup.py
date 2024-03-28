86fa35928156b0fc9a4b303f4ed4a429f0840c1c
Den <deniskarmazen@gmail.com>
2024-03-27 18:23:24 -0700

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 from setuptools import setup
+
+
+def read_version_info():
+    version_info = {}
+    with open("monday_async/_version.py") as file:
+        exec(file.read(), version_info)
+    return version_info
+
+
+version = read_version_info()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="monday_async",
+    version=version["__version__"],
+    description='An asynchronous Python client library for monday.com',
+    install_requires=requirements
+)
