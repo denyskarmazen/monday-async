dfe9b94a0dd9bd927598a4ba94ab09dacf890403
Den <deniskarmazen@gmail.com>
2024-03-28 11:43:06 -0700

    Fixed CustomResource in custom.py

--- monday_async/resources/custom.py
+++ monday_async/resources/custom.py
@@ -5,8 +5,8 @@ from monday_async.utils.utils import graphql_parse
 class CustomResource(AsyncBaseResource):
     async def execute_custom_query(self, custom_query):
         parsed_query = graphql_parse(custom_query)
-        return self.client.execute(parsed_query)
+        return await self.client.execute(parsed_query)
 
     async def execute_custom_file_query(self, custom_query):
         parsed_query = graphql_parse(custom_query)
-        return self.file_upload_client.execute(parsed_query)
+        return await self.file_upload_client.execute(parsed_query)
