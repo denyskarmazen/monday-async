cc5fa1137860b398c21b4dba586a072a96124f5b
Den <deniskarmazen@gmail.com>
2024-04-09 16:20:15 -0700

    Revert "Added updates resource."
    This reverts commit 05e0df5555c37fa2d0359001303657e8fb6be397.

--- setup.py
+++ setup.py
@@ -10,21 +10,12 @@ def read_version_info():
 
 version = read_version_info()
 
-
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
-    name="monday-async",
-    author="JUSTFUN0368",
-    author_email="deniskarmazen@gmail.com",
+    name="monday_async",
     version=version["__version__"],
     description='An asynchronous Python client library for monday.com',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=requirements,
-    python_requires=">=3.10"
+    install_requires=requirements
 )
